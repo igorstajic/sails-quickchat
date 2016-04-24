8a160d6b462a78f7a58efb00c92864e6f7f07ca3
Igor Stajic <istajic@aortasoftware.com>
2016-04-24 11:55:35 +0200

    Added initial ember app

--- api/models/Chat.js
+++ api/models/Chat.js
@@ -8,7 +8,13 @@
 module.exports = {
 
   attributes: {
-
+    user: {
+      type: 'string',
+      required: true
+    },
+    message: {
+      type: 'string',
+      required: true
+    }
   }
 };
-
